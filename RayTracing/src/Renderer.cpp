2c3af66b47714a7a04700742e2ad82b889ff1a54
athirazizi <aziziathir@gmail.com>
2023-03-11 12:26:43 +0000

    FIXED ALLOCATION ERROR

--- RayTracing/src/Renderer.cpp
+++ RayTracing/src/Renderer.cpp
@@ -17,20 +17,17 @@ void Renderer::OnResize(uint32_t width, uint32_t height)
 		m_FinalImage = std::make_shared<Walnut::Image>(width, height, Walnut::ImageFormat::RGBA);
 	}
 
-	// delete and reallocate the data
 	delete[] m_ImageData;
 	m_ImageData = new uint32_t[width * height];
 }
 
 void Renderer::Render()
 {
-	// render every pixel 
-	for (uint32_t i = 0; i < m_FinalImage->GetWidth() * m_FinalImage->GetWidth(); i++)
+	for (uint32_t i = 0; i < m_FinalImage->GetWidth() * m_FinalImage->GetHeight(); i++)
 	{
 		m_ImageData[i] = Walnut::Random::UInt();
 		m_ImageData[i] |= 0xff000000;
 	}
 
-	// set data, which uploads to the GPU
 	m_FinalImage->SetData(m_ImageData);
 }
--- RayTracing/src/Renderer.h
+++ RayTracing/src/Renderer.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include "Walnut/Image.h" // for Render()
+#include "Walnut/Image.h"
 
-#include <memory> // for shared pointers
+#include <memory>
 
 class Renderer
 {
@@ -10,10 +10,10 @@ public:
 	Renderer() = default;
 
 	void OnResize(uint32_t width, uint32_t height);
-	void Render(); // renders every pixel 
+	void Render();
 
-	std::shared_ptr<Walnut::Image>GetFinalImage() const { return m_FinalImage; }
+	std::shared_ptr<Walnut::Image> GetFinalImage() const { return m_FinalImage; }
 private:
-	std::shared_ptr<Walnut::Image>m_FinalImage;
+	std::shared_ptr<Walnut::Image> m_FinalImage;
 	uint32_t* m_ImageData = nullptr;
 };
